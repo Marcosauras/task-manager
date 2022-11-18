03e3d5394975effbf06c27a2c857b9bfaebd1028
eSTee3 <tagsxa@yahoo.com>
2022-11-17 21:14:45 -0600

    new stuff

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
- 
-console.log(dateTime)
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
